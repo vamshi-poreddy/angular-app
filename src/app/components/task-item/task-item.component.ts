058d4bc0b4911aed6bb7b7edd9c5755cf4cbce95
Vamshi Poreddy <80277493+vamshi-poreddy@users.noreply.github.com>
2024-03-05 00:17:44 +0000

    updated functions

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -15,10 +15,15 @@ export class TaskItemComponent {
   @Input()
   task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleRemidner: EventEmitter<Task> = new EventEmitter()
 
   faTimes = faTimes;
   onDelete(task:Task){
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task:Task){
+    this.onToggleRemidner.emit(task);
   }
 
 }
